bf74694a13f329dea68dc7a90e42c13f4f280ad6
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-03-18 18:42:32 -0500

    API calls yay!

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("happy Monday!")
+async function main() {
+    try {
+        let response = await fetch('https://jsonplaceholder.typicode.com/todos/2');
+
+        if (!response.ok) {
+            throw new Error('I AM NOT OKAY!');
+        }
+
+        let todo = await response.json();
+
+        if (todo.id != 1) {
+            throw new Error('Not the right todo!');
+        }
+
+        console.log(todo);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main();
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+//     .catch(error => console.log(error));
